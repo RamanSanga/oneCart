3cd657c8815c06f7f562771f0896459eb4521f23
RamanSanga <ramansanga63@gmail.com>
2026-01-28 17:42:17 +0530

    Update Add.jsx

--- Admin/src/pages/Add.jsx
+++ Admin/src/pages/Add.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
 import Nav from "../component/Nav.jsx";
-import SideBar from "../component/SideBar.jsx";
+import SideBar from "../component/Sidebar.jsx";
 import uploadIcon from "../assets/upload.png";
 import { authDataContext } from "../context/AuthContext.jsx";
 
