f743ea400feadcae3e63e82a8ad9d09db5295737
RamanSanga <ramansanga63@gmail.com>
2026-01-28 15:40:21 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import AuthContext from './Context/authContext.jsx'
-import UserContext from './Context/userContext.jsx'
-import ShopContext from './Context/shopContext.jsx'
+import AuthContext from './Context/AuthContext.jsx'
+import UserContext from './Context/UserContext.jsx'
+import ShopContext from './Context/ShopContext.jsx'
 import "./animations.css";
 
 
