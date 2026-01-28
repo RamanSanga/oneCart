edc12eb2ee749d8a4a98680d752f685f4e14095c
RamanSanga <ramansanga63@gmail.com>
2026-01-28 17:26:58 +0530

    Update relatedProduct.jsx

--- Frontend/src/component/relatedProduct.jsx
+++ Frontend/src/component/relatedProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { shopDataContext } from "../Context/shopContext";
+import { shopDataContext } from "../Context/ShopContext";
 import { useNavigate } from "react-router-dom";
 import Card from "./Card";
 
